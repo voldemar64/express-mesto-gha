e2050b02ca768b5ba29117b125c0495d2042b569
Vladimir Kostyuk <vodemar22864@gmail.com>
2022-07-19 09:46:36 +0300

    added: errors' status

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,14 @@
 const User = require('../models/user');
+const {
+  SERVER_ERROR_STATUS,
+  NOTFOUND_ERROR_STATUS,
+  VALIDATION_ERROR_STATUS,
+} = require('../utils/errors');
 
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then((users) => res.status(200).send({ data: users }))
-    .catch(() => res.status(500).send({ message: 'Список пользователей не получен.' }));
+    .catch(() => res.status(SERVER_ERROR_STATUS).send({ message: 'Список пользователей не получен.' }));
 };
 
 module.exports.getCurrentUser = (req, res) => {
@@ -11,15 +16,15 @@ module.exports.getCurrentUser = (req, res) => {
   User.findById(userId)
     .then((user) => {
       if (!user) {
-        return res.status(404).send({ message: 'Пользователь по указанному _id не найден.' });
+        return res.status(NOTFOUND_ERROR_STATUS).send({ message: 'Пользователь по указанному _id не найден.' });
       }
       return res.status(200).send({ data: user });
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        return res.status(400).send({ message: 'Передан некорректный _id пользователя.' });
+        return res.status(VALIDATION_ERROR_STATUS).send({ message: 'Передан некорректный _id пользователя.' });
       }
-      return res.status(500).send({ message: 'Произошла неизвестная ошибка.' });
+      return res.status(SERVER_ERROR_STATUS).send({ message: 'Произошла неизвестная ошибка.' });
     });
 };
 
@@ -29,9 +34,9 @@ module.exports.createUser = (req, res) => {
     .then((user) => res.status(201).send({ data: user }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        return res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя.' });
+        return res.status(VALIDATION_ERROR_STATUS).send({ message: 'Переданы некорректные данные при создании пользователя.' });
       }
-      return res.status(500).send({ message: 'Произошла неизвестная ошибка.' });
+      return res.status(SERVER_ERROR_STATUS).send({ message: 'Произошла неизвестная ошибка.' });
     });
 };
 
@@ -41,18 +46,18 @@ module.exports.patchUser = (req, res) => {
   User.findByIdAndUpdate(ownerId, { name, about }, { new: true, runValidators: true })
     .then((user) => {
       if (!user) {
-        return res.status(404).send({ message: 'Пользователь с указанным _id не найден.' });
+        return res.status(NOTFOUND_ERROR_STATUS).send({ message: 'Пользователь с указанным _id не найден.' });
       }
       return res.status(200).send({ data: user });
     })
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        return res.status(400).send({ message: 'Переданы некорректные данные при обновлении профиля.' });
+        return res.status(VALIDATION_ERROR_STATUS).send({ message: 'Переданы некорректные данные при обновлении профиля.' });
       }
       if (err.name === 'CastError') {
-        return res.status(400).send({ message: 'Передан некорректный _id пользователя.' });
+        return res.status(VALIDATION_ERROR_STATUS).send({ message: 'Передан некорректный _id пользователя.' });
       }
-      return res.status(500).send({ message: 'Произошла неизвестная ошибка.' });
+      return res.status(SERVER_ERROR_STATUS).send({ message: 'Произошла неизвестная ошибка.' });
     });
 };
 
@@ -62,17 +67,17 @@ module.exports.patchAvatar = (req, res) => {
   User.findByIdAndUpdate(ownerId, { avatar }, { new: true, runValidators: true })
     .then((user) => {
       if (!user) {
-        return res.status(404).send({ message: 'Пользователь с указанным _id не найден.' });
+        return res.status(NOTFOUND_ERROR_STATUS).send({ message: 'Пользователь с указанным _id не найден.' });
       }
       return res.status(200).send({ data: user });
     })
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        return res.status(400).send({ message: 'Переданы некорректные данные при обновлении аватара.' });
+        return res.status(VALIDATION_ERROR_STATUS).send({ message: 'Переданы некорректные данные при обновлении аватара.' });
       }
       if (err.name === 'CastError') {
-        return res.status(400).send({ message: 'Передан некорректный _id пользователя.' });
+        return res.status(VALIDATION_ERROR_STATUS).send({ message: 'Передан некорректный _id пользователя.' });
       }
-      return res.status(500).send({ message: 'Произошла неизвестная ошибка.' });
+      return res.status(SERVER_ERROR_STATUS).send({ message: 'Произошла неизвестная ошибка.' });
     });
 };
