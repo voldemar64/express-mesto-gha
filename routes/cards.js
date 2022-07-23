a17be6bc79868b1e9f8a0196586b16b478fd06c0
Vladimir Kostyuk <vodemar22864@gmail.com>
2022-07-23 15:33:34 +0300

    fixed: params card id

--- routes/cards.js
+++ routes/cards.js
@@ -13,8 +13,8 @@ const {
 
 router.get('/', getCards);
 router.post('/', cardValidation, createCard);
-router.put('/:cardId/likes', cardIdValidation, likeCard);
-router.delete('/:cardId/likes', cardIdValidation, dislikeCard);
-router.delete('/:cardId', cardIdValidation, deleteCard);
+router.put('/:id/likes', cardIdValidation, likeCard);
+router.delete('/:id/likes', cardIdValidation, dislikeCard);
+router.delete('/:id', cardIdValidation, deleteCard);
 
 module.exports = router;
